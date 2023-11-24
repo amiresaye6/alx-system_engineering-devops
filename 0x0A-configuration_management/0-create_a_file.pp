c58df4d3205928fec501b81e55087fa89b1c6d7b
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-24 19:25:13 +0200

    testing changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# i love Puppet
+file {'/tmp/school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
