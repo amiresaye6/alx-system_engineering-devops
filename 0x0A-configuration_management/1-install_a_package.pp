1fbd30daea888ba152e31ab1f88b79c8f2f8e0cf
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-24 19:41:18 +0200

    finally

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install  flask
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    path    => ['/usr/bin'],
+    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
