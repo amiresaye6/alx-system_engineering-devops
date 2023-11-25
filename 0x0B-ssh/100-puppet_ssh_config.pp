0cbf37561b1747bfd1bb861f353276463d7213d8
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-25 19:25:55 +0200

    debug

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,12 +4,12 @@ include stdlib
 
 file_line { 'Refuse to authenticate using a password':
   ensure => present,
-  path   => '/root/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Use private key':
   ensure => present,
-  path   => '/root/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
 }
