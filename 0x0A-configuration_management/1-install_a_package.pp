2d30e35e48a370b7da57ae5a6a4623ad3e09ea0f
Amir Alsayed <amyralsyd367@gmail.com>
2023-11-29 15:39:51 +0200

    debuging

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install  flask
-exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    path    => ['/usr/bin'],
-    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
