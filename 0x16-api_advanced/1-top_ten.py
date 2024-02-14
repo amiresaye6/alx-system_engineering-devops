61b3f178b4c31603c3c007b1545ec105e5d8eeed
Amir Alsayed <amyralsyd367@gmail.com>
2024-02-14 15:44:18 +0200

    d

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,10 @@ def top_ten(subreddit):
     first 10 hot posts listed for a given subreddit.
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
         "User-Agent": "0x16. API_advanced-e_kiminza"
-        }
+    }
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
